4a3cc4bd1bd2be11fe2ed434365f2f835e2dde9d
dj-orp <david.vaseljr@hotmail.com>
2022-10-03 23:30:51 -0300

    Initializing animation scroll and finishing then

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('div a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 500)
+})
